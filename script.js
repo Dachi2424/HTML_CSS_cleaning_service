590332b75eb51271413aafde71a6a2e88f714c6e
Dachi Darjania <dachidar19@gmail.com>
2025-08-07 18:24:59 +0400

    davamate

--- script.js
+++ script.js
@@ -2,9 +2,22 @@ function openBurgerMenu() {
   const menu = document.querySelector('.mobile-navigation');
   menu.style.display = 'unset';
 }
+
 function closeBurgerMenu() {
   const menu = document.querySelector('.mobile-navigation');
   menu.style.display = 'none';
+}
+
+function showBackground(){
+  const dark_background = document.querySelector(".dark-background");
+  dark_background.style.display = "unset";
+}
+
+function closeSelect(){
+  const grid_child4 = document.querySelector(".grid-child4");
+  const dark_background = document.querySelector(".dark-background");
+  dark_background.style.display = "none";
+  grid_child4.classList.remove("active");
 }
 
 
